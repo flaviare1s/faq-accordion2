8725ffbc60a850e9ce1b6b2a59f4c92fc6a8a77b
Flávia Reis <flaviarster@gmail.com>
2024-02-14 13:29:22 -0300

    Adjusting

--- src/script.js
+++ src/script.js
@@ -1,6 +1,9 @@
 function initAccordion() {
     const accordionList = document.querySelectorAll('.js-accordion dt')
 
+    accordionList[0].classList.add('ativo')
+    accordionList[0].nextElementSibling.classList.add('ativo')
+
     function activeAccordion() {
         this.classList.toggle('ativo')
         this.nextElementSibling.classList.toggle('ativo')
