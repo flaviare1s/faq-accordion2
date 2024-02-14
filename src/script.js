9b9e70a1f2ffa8dfaa4815a2ba63561c61c0b715
Flávia Reis <flaviarster@gmail.com>
2024-02-14 13:12:04 -0300

    Making the scripts

---
+++ src/script.js
@@ -0,0 +1,16 @@
+function initAccordion() {
+    const accordionList = document.querySelectorAll('.js-accordion dt')
+
+    function activeAccordion() {
+        this.classList.toggle('ativo')
+        this.nextElementSibling.classList.toggle('ativo')
+    }
+
+    accordionList.forEach((item) => {
+        item.addEventListener('click', activeAccordion)
+    })
+
+}
+
+initAccordion()
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
         light_pink: 'hsl(275, 100%, 97%)',
         grayish_purple: 'hsl(292, 16%, 49%)',
         dark_purple: 'hsl(292, 42%, 14%)',
+        pink: '#AD28EB',
       },
       fontFamily: {
         'Work_Sans': ['Work Sans', 'sans-serif'],
